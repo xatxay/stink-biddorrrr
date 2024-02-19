80d6eb25d564e4f03e4664b97dcb800f2bbd60a1
xatxay <xatxaysmk@gmail.com>
2024-02-19 14:57:14 -0600

    changed ticker

--- src/main.rs
+++ src/main.rs
@@ -169,13 +169,13 @@ fn calculate_position(price: &f64, symbol: &str) -> Option<FormattedPosition> {
             twenty_five_percent_size: format!("{:.1}", size.twenty_five_percent_size),
             thirty_percent_size: format!("{:.1}", size.thirty_percent_size),
         },
-        "WLDUSDT" => FormattedPosition {
-            twenty_percent_price: format!("{:.3}", price.twenty_percent_price),
-            twenty_five_percent_price: format!("{:.3}", price.twenty_five_percent_price),
-            thirty_percent_price: format!("{:.3}", price.thirty_percent_price),
-            twenty_percent_size: format!("{:.1}", size.twenty_percent_size),
-            twenty_five_percent_size: format!("{:.1}", size.twenty_five_percent_size),
-            thirty_percent_size: format!("{:.1}", size.thirty_percent_size),
+        "AGIXUSDT" => FormattedPosition {
+            twenty_percent_price: format!("{:.5}", price.twenty_percent_price),
+            twenty_five_percent_price: format!("{:.5}", price.twenty_five_percent_price),
+            thirty_percent_price: format!("{:.5}", price.thirty_percent_price),
+            twenty_percent_size: format!("{}", size.twenty_percent_size.round()),
+            twenty_five_percent_size: format!("{}", size.twenty_five_percent_size.round()),
+            thirty_percent_size: format!("{}", size.thirty_percent_size.round()),
         },
         _ => return None,
     };
@@ -305,7 +305,7 @@ async fn main() {
         env::var("BATCH_CANCEL_ORDER_URL").expect("batch cancel order url is missing");
 
     loop {
-        let symbols = vec!["BEAMUSDT", "INJUSDT", "WLDUSDT"];
+        let symbols = vec!["BEAMUSDT", "INJUSDT", "AGIXUSDT"];
         let futures = symbols.into_iter().map(|symbol| get_kline(&symbol));
         let results = futures::future::join_all(futures).await;
         let mut cancel_order_data: Vec<CancelOrderData> = Vec::new();
