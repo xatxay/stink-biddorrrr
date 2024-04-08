445640f8898c796bb12a16c9d9661cfe458ca193
xatxay <xatxaysmk@gmail.com>
2024-04-07 19:53:10 -0500

    changed ticker

--- src/main.rs
+++ src/main.rs
@@ -153,13 +153,13 @@ fn calculate_position(price: &f64, symbol: &str) -> Option<FormattedPosition> {
     //when starting the app
 
     let formatted_position = match symbol {
-        "BEAMUSDT" => FormattedPosition {
-            twenty_percent_price: format!("{:.6}", price.twenty_percent_price),
-            twenty_five_percent_price: format!("{:.6}", price.twenty_five_percent_price),
-            thirty_percent_price: format!("{:.6}", price.thirty_percent_price),
-            twenty_percent_size: format!("{:.0}", size.twenty_percent_size.round()),
-            twenty_five_percent_size: format!("{:.0}", size.twenty_five_percent_size.round()),
-            thirty_percent_size: format!("{:.0}", size.thirty_percent_size.round()),
+        "ALTUSDT" => FormattedPosition {
+            twenty_percent_price: format!("{:.4}", price.twenty_percent_price),
+            twenty_five_percent_price: format!("{:.4}", price.twenty_five_percent_price),
+            thirty_percent_price: format!("{:.4}", price.thirty_percent_price),
+            twenty_percent_size: format!("{}", size.twenty_percent_size.round()),
+            twenty_five_percent_size: format!("{}", size.twenty_five_percent_size.round()),
+            thirty_percent_size: format!("{}", size.thirty_percent_size.round()),
         },
         "MANTAUSDT" => FormattedPosition {
             twenty_percent_price: format!("{:.4}", price.twenty_percent_price),
@@ -169,13 +169,13 @@ fn calculate_position(price: &f64, symbol: &str) -> Option<FormattedPosition> {
             twenty_five_percent_size: format!("{:.1}", size.twenty_five_percent_size.round()),
             thirty_percent_size: format!("{:.1}", size.thirty_percent_size.round()),
         },
-        "AGIXUSDT" => FormattedPosition {
-            twenty_percent_price: format!("{:.5}", price.twenty_percent_price),
-            twenty_five_percent_price: format!("{:.5}", price.twenty_five_percent_price),
-            thirty_percent_price: format!("{:.5}", price.thirty_percent_price),
-            twenty_percent_size: format!("{}", size.twenty_percent_size.round()),
-            twenty_five_percent_size: format!("{}", size.twenty_five_percent_size.round()),
-            thirty_percent_size: format!("{}", size.thirty_percent_size.round()),
+        "TAOUSDT" => FormattedPosition {
+            twenty_percent_price: format!("{:.2}", price.twenty_percent_price),
+            twenty_five_percent_price: format!("{:.2}", price.twenty_five_percent_price),
+            thirty_percent_price: format!("{:.2}", price.thirty_percent_price),
+            twenty_percent_size: format!("{:.3}", size.twenty_percent_size.round()),
+            twenty_five_percent_size: format!("{:.3}", size.twenty_five_percent_size.round()),
+            thirty_percent_size: format!("{:.3}", size.thirty_percent_size.round()),
         },
         _ => return None,
     };
@@ -305,7 +305,7 @@ async fn main() {
         env::var("BATCH_CANCEL_ORDER_URL").expect("batch cancel order url is missing");
 
     loop {
-        let symbols = vec!["BEAMUSDT", "MANTAUSDT", "AGIXUSDT"];
+        let symbols = vec!["ALTUSDT", "MANTAUSDT", "TAOUSDT"];
         let futures = symbols.into_iter().map(|symbol| get_kline(&symbol));
         let results = futures::future::join_all(futures).await;
         let mut cancel_order_data: Vec<CancelOrderData> = Vec::new();
